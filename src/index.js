f40edf9ef7e10c4ec330c853befa99473cf0e879
Saif Kotwal <saifkotwal2697@gmail.com>
2019-01-28 07:23:37 +0530

    Kaprekar number (#9)
    * Added function to find the nth catalan number
    
    * Added curly brackets for if condition
    
    * Added isKaprekar function
    
    * Add curly brackets
    
    * adding catalan to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 /* Types of Numbers */
 
-const armstrong = require('./armstrong'),
-	automorphic = require('./automorphic'),
-	fibonacci = require('./fibonacci'),
-	magic = require('./magic'),
-	neon = require('./neon'),
-	palindrome = require('./palindrome'),
-	prime = require('./prime'),
-	factorial = require('./factorial');
+const armstrong = require("./armstrong"),
+	automorphic = require("./automorphic"),
+	fibonacci = require("./fibonacci"),
+	magic = require("./magic"),
+	neon = require("./neon"),
+	palindrome = require("./palindrome"),
+	prime = require("./prime"),
+	factorial = require("./factorial"),
+	catalan = require("./catalan"),
+	kaprekar = require("./kaprekar");
 
 /* Mathematical Utilities */
 
-const gcd = require('./gcd'),
-	isEven = require('./isEven'),
-	isOdd = require('./isOdd'),
-	lcm = require('./lcm'),
-	length = require('./length'),
-	max = require('./max'),
-	min = require('./min'),
-	sort = require('./sort'),
-	sum = require('./sum');
+const gcd = require("./gcd"),
+	isEven = require("./isEven"),
+	isOdd = require("./isOdd"),
+	lcm = require("./lcm"),
+	length = require("./length"),
+	max = require("./max"),
+	min = require("./min"),
+	sort = require("./sort"),
+	sum = require("./sum");
 
 /* M - mathball utility object */
 
@@ -32,6 +34,8 @@ module.exports = {
 	isNeon: neon.isNeon,
 	isPalindrome: palindrome.isPalindrome,
 	isPrime: prime.isPrime,
+	catalan: catalan.catalan,
+	isKaprekar: kaprekar.isKaprekar,
 	gcd,
 	isEven,
 	isOdd,
---
+++ src/kaprekar/index.js
@@ -0,0 +1,35 @@
+/* Kaprekar Number
+ * Function: isKaprekar()
+ */
+
+function validate(arg) {
+	if (typeof arg != "number" || arg < 1 || !Number.isInteger(arg)) {
+		throw new TypeError(
+			`Invalid argument recieved: ${JSON.stringify(
+				arg
+			)}\n'isKaprekar()' only accepts a positive integer!\n`
+		);
+	}
+}
+
+exports.isKaprekar = num => {
+	validate(num);
+	if (num == 1) {
+		return true;
+	}
+	var sqNum = num * num;
+	var countDigits = num.toString().length;
+	var rDigits = 0;
+	while (rDigits < countDigits) {
+		rDigits++;
+		var eqParts = Math.pow(10, rDigits);
+		if (eqParts == num) {
+			continue;
+		}
+		var sum = Math.floor(sqNum / eqParts) + (sqNum % eqParts);
+		if (sum == num) {
+			return true;
+		}
+	}
+	return false;
+};
