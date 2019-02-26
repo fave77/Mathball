caddb38eea57e4b46dd90cefb2e65ad0d87aca3d
Indrajit1997 <senindrajit1997@gmail.com>
2019-02-26 11:31:05 +0530

    bell number fixed

---
+++ src/bell/index.js
@@ -0,0 +1,35 @@
+/* N-th bell Number
+ * Function: bell()
+ */
+
+const validate = require('../validation/non-negative-integer');
+
+function bell(number){
+
+    let bell = [...Array(number+1)].map(x => Array(number+1).fill(0));
+
+    bell[0][0] = 1;
+
+    for( let i=1; i<=number; i++ ){
+
+        bell[i][0] = bell[i-1][i-1];
+
+        for( let j=1; j<=i; j++ ){
+
+            bell[i][j] = bell[i-1][j-1] + bell[i][j-1];
+
+        }
+
+    }
+
+    return bell[number][0];
+
+}
+
+exports.bell = num => {
+
+    validate(num, 'bell');
+
+    return bell(num);
+    
+}
