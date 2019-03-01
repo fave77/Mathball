e7ddc9a20d77deb86a8e760ca575de4cdbf5cc7a
Abdus Samad Azad <azad3652@gmail.com>
2019-03-01 11:32:48 +0530

    Added new Mathematical Utility: frequency()

---
+++ src/frequency/index.js
@@ -0,0 +1,29 @@
+/**
+ * Frequency
+ *
+ * `frequency()` accepts two arguments and return the occurance of second argument in first
+ * @param {string|number|string[]|number[]} arg - Accepts either a String or an Array
+ * @param {string|number} occurence - The phrase or array item you want to find
+ */
+module.exports = (arg, occurence) => {
+  if (arg && arg.constructor === Array) return (arg = arg.filter(val => val === occurence).length);
+  else if (arg && typeof arg === 'string') return findStrOccurence(arg, occurence);
+  else throw new TypeError(`Invalid argument received: ${JSON.stringify(arg)}\nfrequency() only accepts an Array or a String`);
+};
+
+/**
+* Find the occurence count of a phrase
+* @param {string} str
+* @param {string} occurance
+*/
+const findStrOccurence = (str, occurance) => {
+  let count = 0,
+      flag = -1;
+
+  do {
+      flag = str.indexOf(occurance, flag + 1);
+      flag !== -1 ? count++ : null;
+  } while (flag !== -1);
+
+  return count;
+};
