e95c7d220f99b00836f84524ade419a171b5dfd7
Sarthak Sarangi <sarthaks793@gmail.com>
2019-03-02 21:23:46 +0530

    Padovan nth number (#48)
    * Padovan nth number
    
    * Test and readme

---
+++ src/padovan/index.js
@@ -0,0 +1,24 @@
+/* Padovan Number
+ * Function: padovan()
+ */
+
+const validate = require("../validation/positive-integer");
+
+function pad(num) {
+	let pPrevPrev = 1,
+		pPrev = 1,
+		pCurr = 1,
+		pNext = 1;
+	for (let i = 3; i <= num; i++) {
+		pNext = pPrevPrev + pPrev;
+		pPrevPrev = pPrev;
+		pPrev = pCurr;
+		pCurr = pNext;
+	}
+	return pNext;
+}
+
+exports.padovan = num => {
+	validate(num, "padovan");
+	return pad(num);
+};
---
+++ test/padovan-spec.js
@@ -0,0 +1,36 @@
+const assert = require("assert"),
+	padovan = require("../src/padovan").padovan;
+
+describe("[Function: padovan]", () => {
+	it("should be a function", () => {
+		assert.strictEqual(typeof padovan, "function");
+	});
+
+	it("should return a number when a positive integer is passed", () => {
+		assert.strictEqual(typeof padovan(1), "number");
+	});
+
+	it("should return '4' when '6' is passed", () => {
+		assert.strictEqual(padovan(6), 4);
+	});
+
+	it("should return '49' when '15' is passed", () => {
+		assert.strictEqual(padovan(15), 49);
+	});
+
+	it("should throw an error when a negative number is passed", () => {
+		assert.throws(() => padovan(-20), TypeError);
+	});
+
+	it("should throw an error when a floating point is passed", () => {
+		assert.throws(() => padovan(31.101996), TypeError);
+	});
+
+	it("should throw an error when a string is passed", () => {
+		assert.throws(() => padovan("31"), TypeError);
+	});
+
+	it("should throw an error when no arguments passed", () => {
+		assert.throws(() => padovan(), TypeError);
+	});
+});
