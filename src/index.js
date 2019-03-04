515d56c241fac5baf36e596cc612c9e11799a470
bharati-s <37044069+bharati-s@users.noreply.github.com>
2019-03-05 00:32:34 +0530

    Degree to Radians #37
    Degree to Radians #37

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const validate = require('../validation/integer');
+const pi = 22/7;
+
+function toRadian(num) {
+	return (num * (pi/180));
+	 
+}
+
+exports.toRadian = num => {
+	validate(num, 'toRadian');
+	return toRadian(num);
+};
+
