7ef49969f67cc180a12b1403869ed7e0efe7439b
bharati-s <37044069+bharati-s@users.noreply.github.com>
2019-03-05 00:27:41 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,74 +1,13 @@
-/* Number Checkers */
-const armstrong = require('./armstrong'),
-	automorphic = require('./automorphic'),
-	kaprekar = require('./kaprekar'),
-	lucky = require('./lucky'),
-	neon = require('./neon'),
-	palindrome = require('./palindrome'),
-	perfect = require('./perfect'),
-	prime = require('./prime'),
-	smith = require('./smith'),
-	isDeficient = require('./isDeficient'),
-	carmichael = require('./carmichael');
+const validate = require('../validation/integer');
+const pi = 22/7;
 
-/* Find nth Number */
-const bell = require('./bell'),
-	catalan = require('./catalan'),
-	factorial = require('./factorial'),
-	fibonacci = require('./fibonacci'),
-	magic = require('./magic'),
-	padovan = require('./padovan'),
-	smart = require('./smart');
+function toRadian(num) {
+	return (num * (pi/180));
+	 
+}
 
-/* Mathematical Utilities */
-const frequency = require('./frequency'),
-  gcd = require('./gcd'),
-	isEven = require('./isEven'),
-	isOdd = require('./isOdd'),
-	lcm = require('./lcm'),
-	length = require('./length'),
-	max = require('./max'),
-	min = require('./min'),
-	modInv = require('./modInv'),
-	sort = require('./sort'),
-	sum = require('./sum');
-
-/* Helper Methods */
-const performance = require('./performance'),
-	pipe = require('./pipe');
-
-/* M - Mathball Object */
-module.exports = {
-	isArmstrong: armstrong.isArmstrong,
-	isAutomorphic: automorphic.isAutomorphic,
-	bell: bell.bell,
-	catalan: catalan.catalan,
-	factorial: factorial.factorial,
-	fibonacci: fibonacci.fibonacci,
-	padovan: padovan.padovan,
-	gcd,
-	isEven,
-	isOdd,
-	isKaprekar: kaprekar.isKaprekar,
-	lcm,
-	length,
-	isLucky: lucky.isLucky,
-	magic: magic.magic,
-	max,
-	min,
-	isNeon: neon.isNeon,
-	isPalindrome: palindrome.isPalindrome,
-	isPerfect: perfect.isPerfect,
-	performance,
-	pipe,
-	isPrime: prime.isPrime,
-	rangePrime: prime.rangePrime,
-	smart: smart.smart,
-	isSmith: smith.isSmith,
-	sort,
-	sum,
-	modInv: modInv.modInv,
-	frequency,
-	isDeficient,
-	isCarmichael: carmichael.isCarmichael
+exports.toRadian = num => {
+	validate(num, 'toRadian');
+	return toRadian(num);
 };
+
