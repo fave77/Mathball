2f672ecb4c1ba4ab71ee4b47eebaa1759b51b13e
Shubhayu Das <43082352+sateslayer@users.noreply.github.com>
2019-03-05 21:41:34 +0530

    pow-spec.js

---
+++ test/pow-spec.js
@@ -0,0 +1,40 @@
+const assert = require('assert'),
+    pow = require('../src/pow');
+
+describe('[Function: pow', () =>{
+    it('should be a function', () =>{
+        assert.strictEqual(typeof pow, 'function');
+    });
+
+    it('should return a number when an array of number is passed', () => {
+        assert.strictEqual(typeof pow([1, 1, 1]), 'number');
+    });
+
+    it('should throw an error when a floating point is passed to modulo', () => {
+		assert.throws(() => pow(5,2, 1.2465), TypeError);
+    });
+
+    it('should throw an error when a negative number is passed', () => {
+		assert.throws(() => pow(-20), TypeError);
+    });
+    
+    it('should return \'25\' when \'[5, 2]\' is passed', () =>{
+        assert.strictEqual(pow([5, 2]), 25);
+    });
+
+    it('should return \'5\' when \'[5, 3, 6]\' is passed', () =>{
+        assert.strictEqual(pow([5, 3, 6]), 5);
+    });
+
+    it('should return \'0\' when \'[5, 3, 1]\' is passed', () =>{
+        assert.strictEqual(pow([5, 3, 1]), 0);
+    });
+
+    it('should throw an error when no arguments passed', () => {
+		assert.throws(() => pow(), TypeError);
+    });
+    
+    it('should throw an error when one arguments is passed', () => {
+		assert.throws(() => pow(5), TypeError);
+	});
+});
