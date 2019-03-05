038f0af654a39b11c7dbce5ec6bc040f0b933de1
Shubhayu Das <43082352+sateslayer@users.noreply.github.com>
2019-03-05 22:56:05 +0530

    Updated according to Codacy report

--- src/pow/index.js
+++ src/pow/index.js
@@ -5,7 +5,9 @@ const validate = require('../validation/number-array');
 function pow(a, b, c = Number.MAX_SAFE_INTEGER)
 {
     if(b == 0)
-        return 1;
+		{
+			return 1;
+		}
     
     var y;
     if(b%2 == 0){
@@ -21,8 +23,12 @@ function pow(a, b, c = Number.MAX_SAFE_INTEGER)
 
 module.exports = arr => {
     validate(arr, 'pow');
-    if( arr[2] != undefined)
-        return pow(arr[0], arr[1], arr[2]);
+    if( arr.length == 3)
+		{
+			return pow(arr[0], arr[1], arr[2]);
+		}
     else
-        return pow(arr[0], arr[1]);
+		{
+			return pow(arr[0], arr[1]);
+		}
 }
