e4f928a61bf76511114d5d67b45fb0f56a713306
Priyabrata Biswas <pbiswas101b@gmail.com>
2019-03-05 19:07:38 +0530

    Update index.js

--- src/harshad/index.js
+++ src/harshad/index.js
@@ -6,7 +6,7 @@ const validate = require('../validation/positive-integer');
 
 function isHarshad(num){
 	let sum = 0;
-	for(let i=num;i>0;i/=10){
+	for(let i=num;i>0;i=parseInt(i/10,10)){
 		sum+=i%10;
 	}
 	return num%sum === 0;
