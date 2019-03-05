28a80615567cad4e45d4b5f8707b0c5d7a08d21a
Shubhayu Das <43082352+sateslayer@users.noreply.github.com>
2019-03-05 21:40:54 +0530

    Adding pow.js folder

---
+++ src/pow/index.js
@@ -0,0 +1,28 @@
+
+const validate = require('../validation/number-array');
+
+
+function pow(a, b, c = Number.MAX_SAFE_INTEGER)
+{
+    if(b == 0)
+        return 1;
+    
+    var y;
+    if(b%2 == 0){
+        y = pow(a, b/2, c);
+        y = (y*y)%c;
+    }
+    else{
+        y = a%c;
+        y = (y* pow(a, b-1, c)%c)%c;
+    }
+    return Math.trunc((y+c)%c);
+}
+
+module.exports = arr => {
+    validate(arr, 'pow');
+    if( arr[2] != undefined)
+        return pow(arr[0], arr[1], arr[2]);
+    else
+        return pow(arr[0], arr[1]);
+}
