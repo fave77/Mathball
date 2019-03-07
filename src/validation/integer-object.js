3d72e0dd776f2a0bbacfe3ad7b9631ebf5c4dd97
devRD <35831931+devRD@users.noreply.github.com>
2019-03-07 23:34:51 +0530

    Update integer-object.js

--- src/validation/integer-object.js
+++ src/validation/integer-object.js
@@ -1,6 +1,7 @@
 //validate integer or object
 
 module.exports = (arg, func) =>  {
-	if(typeof arg === 'boolean' || typeof arg === 'string' || arg.constructor === Array)
+	if(typeof arg === 'boolean' || typeof arg === 'string' || arg.constructor === Array){
 		throw new TypeError(`Invalid argument received: ${JSON.stringify(arg)}\n'${func}()' only accepts an integer or an object!\n`);
-};
+	}
+}
