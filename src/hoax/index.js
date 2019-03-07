e0c75f313d0ae37c05edda8608dc9b9ef5de10f8
pydevsg <ghoshsudipto1129@gmail.com>
2019-03-07 19:52:12 +0530

    issue solver

--- src/hoax/index.js
+++ src/hoax/index.js
@@ -5,30 +5,36 @@ const validate = require('../validation/positive-integer');
 function primeFactors(num){
 	let res=[];
 	if(num%2===0){
-		while(num%2===0)
+		while(num%2===0){
 			num=num/2;
+		}
 		res.push(2);
 	}
 	for(let i=3;i<=Math.sqrt(num);i=i+2){
-		if(num%i==0){
-			while(num%i==0)
+		if(num%i===0){
+			while(num%i===0){
 				num=num/i;
+			}
 			res.push(i);
 		}
 	}
-	if(n>2)
-		res.push(num)
+	if(num>2){
+		res.push(num);
+	}
 	return res;
 }
 function isHoax(num){
 	let pf=[];
 	pf=primeFactors(num);
-	if(pf[0]==num)
+	if(pf[0]===num){
 		return false;
+	}
 	let allPfsum=0;
 	for(let i=0;i<pf.length;i++){
 		let pfSum;
-		for(pfSum=0;pfSum[i]>0;pfSum+=pf[i]%10,pf[i]/=10);
+		for(pfSum=0;pfSum[i]>0;pfSum+=pf[i]%10,pf[i]/=10){
+			;
+		}
 	allPfsum;
 	}
 	let sumN;
