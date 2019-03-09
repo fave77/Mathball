90f6659c4cdf57c1ea51e13e58fad37b3662c575
sateslayer <shubhayu64@gmail.com>
2019-03-09 17:15:33 +0530

    Adding rangePalin(). Issue #76

--- src/palindrome/index.js
+++ src/palindrome/index.js
@@ -1,5 +1,6 @@
 /* Palindromic Number
  * Function: isPalindrome()
+ * Function: rangePal() --> to return all palindromic numbers from 0 to end
  */
 
 const validate = require('../validation/non-negative-integer');
@@ -8,3 +9,32 @@ exports.isPalindrome = num => {
 	validate(num, 'isPalindrome');
 	return num == num.toString().split('').reverse().join('');
 };
+
+exports.rangePal = (start, end) => {
+	validate(start, 'rangePal');
+	validate(end, 'rangePal');
+	
+	if(start > end)
+	{
+		throw new TypeError(
+			`Starting number must be smaller than the ending number!!\n`
+		);
+	}
+	let palinList = [], lim = 0;
+	
+	lim = (end < 10)? end : start;
+
+	for(let i = start; i < lim; i++)
+	{
+		palinList.push(i);
+	}
+
+	for(let i = lim; i < end; i++)
+	{
+		if(i.toString() === i.toString().split('').reverse().join('')){
+			palinList.push(i);
+		}
+	}
+
+	return palinList;
+}
--- test/palindrome-spec.js
+++ test/palindrome-spec.js
@@ -1,5 +1,6 @@
 const assert = require('assert'),
-	isPalindrome = require('../src/palindrome').isPalindrome;
+	isPalindrome = require('../src/palindrome').isPalindrome,
+	rangePal = require('../src/palindrome').rangePal;
 
 describe('[Function: isPalindrome]', () => {
 
@@ -33,6 +34,50 @@ describe('[Function: isPalindrome]', () => {
 
 	it('should throw an error when no arguments passed', () => {
 		assert.throws(() => isPalindrome(), TypeError);
+	});
+
+});
+
+describe('[Function: rangePal]', () => {
+
+	it('should be a function', () => {
+		assert.strictEqual(typeof rangePal, 'function');
+	});
+
+	it('should return a object when a non-negative integers are passed', () => {
+		assert.strictEqual(typeof rangePal(1,5), 'object');
+	});
+
+	it('should throw an error when a negative number is passed', () => {
+		assert.throws(() => rangePal(-20), TypeError);
+	});
+
+	it('should throw an error when a lower limit is larger than upper limit', () => {
+		assert.throws(() => rangePal(25,20), TypeError);
+	});
+
+	it('should throw an error when a negative number is passed', () => {
+		assert.throws(() => rangePal(15,-20), TypeError);
+	});
+
+	it('should throw an error when a floating point is passed', () => {
+		assert.throws(() => rangePal(31.101996), TypeError);
+	});
+
+	it('should throw an error when a string is passed', () => {
+		assert.throws(() => rangePal('31'), TypeError);
+	});
+
+	it('should throw an error when no arguments passed', () => {
+		assert.throws(() => rangePal(), TypeError);
+	});
+
+	it('should throw an error when one argument is passed', () => {
+		assert.throws(() => rangePal(5), TypeError);
+	});
+
+	it('should return \'[0,1,2,3,4,5,6,7,8,9,11]\' when \'0, 18\' are passed', () => {
+		assert.deepEqual(rangePal(0,18), [0,1,2,3,4,5,6,7,8,9,11]);
 	});
 
 });
