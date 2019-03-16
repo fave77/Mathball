c1b0e55d0688b5cb8774354e147f6b14b6f13b0c
Priyabrata Biswas <pbiswas101b@gmail.com>
2019-03-16 21:35:53 +0530

    fixing codacy errors

--- src/find/index.js
+++ src/find/index.js
@@ -3,7 +3,7 @@
 const validate = require('../validation/string');
 
 module.exports = arg => {
-    validate(arg,'find')
+    validate(arg,'find');
    /* return the corresponding function */
     return require(`../${arg}`).find;
   };
