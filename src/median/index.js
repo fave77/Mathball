5632c0c6e6acd963217a91d9e190abfc30411685
Manvi Tyagi <manvityagi770@gmail.com>
2019-03-17 01:50:41 +0530

    adding median func

---
+++ src/median/index.js
@@ -0,0 +1,20 @@
+/* Function: median() */
+
+const validate = require('../validation/number-array');
+
+module.exports = arr => {
+	validate(arr, 'median');
+    arr.sort(function(a,b){
+        return a-b;
+      });
+    
+      var half = Math.floor(arr.length / 2);
+
+      if (arr.length % 2){
+        return arr[half];
+      }
+      else{
+        return (arr[half - 1] + arr[half]) / 2.0;
+      }
+    
+};
