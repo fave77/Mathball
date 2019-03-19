36afbdf34bd620318692c77bae398abfa9b6e4de
sateslayer <shubhayu64@gmail.com>
2019-03-19 14:42:32 +0530

    Adding Fibbinary numbers. Issue #128

---
+++ src/fibbinary/index.js
@@ -0,0 +1,16 @@
+const validate = require('../validation/positive-integer');
+
+function isFibbinary(num){
+    if ((num & (num >> 1)) == 0) {
+        return true;
+    }
+
+    else{
+        return false;
+    }
+}
+
+exports.check = num => {
+    validate(num, 'fibbinary');
+    return isFibbinary(num);
+};
