eab88d21fdc238f50c98eb8bea712414ef986080
sateslayer <shubhayu64@gmail.com>
2019-03-29 22:52:29 +0530

    Cleaning up catalan number code.

--- src/catalan/index.js
+++ src/catalan/index.js
@@ -2,19 +2,11 @@
  * Function: catalan()
  */
 
-const validate = require('../validation/non-negative-integer');
-
-function binomialCoeff(n, k) {
-	let res = 1;
-	for (let i = 0; i < k; i++) {
-		res = res * (n - i);
-		res = res / (i + 1);
-	}
-	return res;
-}
+const validate = require('../validation/non-negative-integer'),
+		nCr = require('../binomial-coeff/index');
 
 exports.find = num => {
 	validate(num, 'catalan');
-	let c = binomialCoeff(2 * num, num);
+	let c = nCr(2 * num, num);
 	return c / (num + 1);
 };
