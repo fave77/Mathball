7bfbde40275b86191b932b10c84f8652c73dbd81
sakshichahal53 <Vinay15dev@>
2019-04-12 03:58:33 +0530

    sort matrix added #152

--- src/testing.js
+++
@@ -1,9 +0,0 @@
-let M = require('mathball');
-
-console.log(M.check('palindrome')(1551)); /* true */
-console.log(M.check('even')(7)); /* false */
-console.log(M.sum([4, 5, 9])); /* 18 */
-
-a=[2,-1,0,9,8];
-b=M.sort(a,true);
-console.log(b);
