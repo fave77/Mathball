ba4129d2a55e8f852b73ce6496efe51f72921b75
Harshavardhan Surisetty <harsha19990226@GMAIL.COM>
2019-05-18 16:57:34 +0530

    Update index.js

--- src/gcd/index.js
+++ src/gcd/index.js
@@ -1,12 +1,23 @@
 /* Function: gcd() */
 
 const validate = require('../validation/integer-array');
+function findGCD(a, b) 
+{ 
+    if (b == 0) 
+       
+       return a; 
 
-function gcd(a, b) {
-  return (a == 0) ? b : gcd(b % a, a);
+    return findGCD(b, a % b);  
+} 
+function gcd(arr) 
+{ 
+    var result = arr[0]; 
+    for (var i = 1; i < arr.length; i++) 
+        result = findGCD(arr[i], result); 
+  
+    return result; 
 }
-
 module.exports = arr => {
 	validate(arr, 'gcd');
-	return arr.reduce((prev, next) =>  gcd(prev, next), arr[0]);
+	return gcd(arr);
 };
