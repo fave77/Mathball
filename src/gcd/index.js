5f3b406222d98c76e137de6c83ef60b75817a4f4
harsha-iiiv <31560965+harsha-iiiv@users.noreply.github.com>
2019-05-18 13:26:30 +0530

    added gcd method

--- src/gcd/index.js
+++ src/gcd/index.js
@@ -1,12 +1,26 @@
 /* Function: gcd() */
 
 const validate = require('../validation/integer-array');
-
-function gcd(a, b) {
-  return (a == 0) ? b : gcd(b % a, a);
+function findGCD(a, b) 
+{ 
+    if (a < b) 
+        return findGCD(b, a); 
+  
+    if (Math.abs(b) < 0.001) 
+        return a; 
+  
+    else
+        return (findGCD(b, a - (a / b) * b)); 
+} 
+function gcd(arr) 
+{ 
+    var result = arr[0]; 
+    for (var i = 1; i < arr.length; i++) 
+        result = findGCD(arr[i], result); 
+  
+    return result; 
 }
-
 module.exports = arr => {
 	validate(arr, 'gcd');
-	return arr.reduce((prev, next) =>  gcd(prev, next), arr[0]);
+	return gcd(arr);
 };
