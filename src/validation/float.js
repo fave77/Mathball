dd123ce473fbd3914f13243df0a0f58282f9424d
Harshavardhan Surisetty <harsha19990226@GMAIL.COM>
2019-05-20 16:47:05 +0530

    Update float.js

--- src/validation/float.js
+++ src/validation/float.js
@@ -4,4 +4,4 @@ module.exports = (arg, func) =>  {
 	if (!(Number(arg) === arg && arg % 1 !== 0)) {
 	  throw new TypeError(`Invalid argument received: ${JSON.stringify(arg)}\n'${func}()' only accept a positive integer!\n`);
       }
-}
+};
