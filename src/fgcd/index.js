b55920c5b1a4729732a56dae8b3313de6d292329
Harshavardhan Surisetty <harsha19990226@GMAIL.COM>
2019-05-20 16:37:48 +0530

    Add files via upload

---
+++ src/fgcd/index.js
@@ -0,0 +1,23 @@
+/* Function: fgcd() */
+
+const validate = require('../validation/float');
+
+
+ function fgcd(a, b) 
+{ 
+     if (a < b) 
+        return fgcd(b, a); 
+  
+     if (Math.abs(b) < 0.001) 
+        return a; 
+  
+    else
+        return (fgcd(b, a - Math.floor(a / b) * b));  
+} 
+ 
+module.exports = (a, b) => {
+	validate(a, 'fgcd');
+	validate(b, 'fgcd');
+	
+  	return Math.round((fgcd(a,b) + 0.00001) * 1000) / 1000;
+};
