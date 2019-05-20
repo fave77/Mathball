1dc90562d3e0ce41e6c830063787b535564a75e0
Harshavardhan Surisetty <harsha19990226@GMAIL.COM>
2019-05-20 16:38:54 +0530

    Add files via upload

---
+++ src/validation/float.js
@@ -0,0 +1,7 @@
+
+
+module.exports = (arg, func) =>  {
+	if (!(Number(arg) === arg && arg % 1 !== 0)) {
+	  throw new TypeError(`Invalid argument received: ${JSON.stringify(arg)}\n'${func}()' only accept a positive integer!\n`);
+      }
+}
