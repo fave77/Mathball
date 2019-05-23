f9f3968e96de3bf23dfee3db50c7e284bb947c50
Harshavardhan Surisetty <harsha19990226@GMAIL.COM>
2019-05-23 14:35:27 +0530

    Update gcd-spec.js

--- test/gcd-spec.js
+++ test/gcd-spec.js
@@ -7,6 +7,7 @@ describe('[Function: gcd]', () => {
 		assert.strictEqual(typeof gcd, 'function');
 	});
 
+
 	it('should return a number when an array of number is passed', () => {
 		assert.strictEqual(typeof gcd([1, 1]), 'number');
 	});
@@ -18,16 +19,20 @@ describe('[Function: gcd]', () => {
 	it('should return \'4\' when \'[24, 28]\' is passed', () => {
 		assert.strictEqual(gcd([24, 28]), 4);
 	});
+	it('should return \'0.012\' when \'0.48, 0.108\' are passed', () => {
+		assert.strictEqual(gcd([0.48, 0.108]), 0.012);
+	});
+
+	it('should return \'2.2\' when \'8.8,6.6\' are passed', () => {
+		assert.strictEqual(gcd([8.8,6.6]), 2.2);
+	});
 
 	it('should throw an error when a negative number is passed', () => {
 		assert.throws(() => gcd(-20), TypeError);
 	});
 
-	it('should throw an error when a floating point is passed', () => {
-		assert.throws(() => gcd(31.101996), TypeError);
-	});
-
-	it('should throw an error when a string is passed', () => {
+	 
+    it('should throw an error when a string is passed', () => {
 		assert.throws(() => gcd('31'), TypeError);
 	});
 
