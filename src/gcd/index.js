7a4ef74d6d73c5fb4136bebd7b06f6fcd114a942
Brendo64 <brendonovich@outlook.com>
2019-06-27 07:38:40 +0800

    Fix for Codacy checks for PR #215
    Implements Codacy requirements to allow it to pass in Pull Request #215

--- src/gcd/index.js
+++ src/gcd/index.js
@@ -4,26 +4,27 @@ const validate = require('../validation/number-array');
 
 function gcd(a, b) {
 	return (a == 0) ? b : gcd(b % a, a);
-}
-
-module.exports = arr => {
-	validate(arr, 'gcd');
-	var posArr = arr.map((value) => (value < 0) ? value * -1 : value)
-	var largestDecimal = posArr.reduce((prev, next) => Math.max(getDecimalCount(prev), getDecimalCount(next)))
-	return posArr.reduce((prev, next) => gcd(prev * Math.pow(10, largestDecimal), next * Math.pow(10, largestDecimal)) / Math.pow(10, largestDecimal), posArr[0]);
 };
 
 const getDecimalCount = value => {
-	var decimals = 0
-	if (value % 1 == 0) {
-		return decimals
+	var decimals = 0;
+	if (value % 1 === 0) {
+		return decimals;
 	}
 	decimals++
 	for (var i = 1; i < 11; i++) {
-		if ((value * Math.pow(10, i)) % 1 != 0) {
-			decimals++
+		if ((value * Math.pow(10, i)) % 1 !== 0) {
+			decimals++;
 		}
-		else { break }
+		else { break; }
 	}
-	return decimals
-}
+	return decimals;
+};
+
+module.exports = arr => {
+	validate(arr, 'gcd');
+	var posArr = arr.map((value) => (value < 0) ? value * -1 : value);
+	var largestDecimal = posArr.reduce((prev, next) => Math.max(getDecimalCount(prev), getDecimalCount(next)));
+	return posArr.reduce((prev, next) => gcd(prev * Math.pow(10, largestDecimal), next * Math.pow(10, largestDecimal)) / Math.pow(10, largestDecimal), posArr[0]);
+};
+
