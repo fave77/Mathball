d275eab28d13068c8135d533c401cc9c098a7090
Harsha <56157392+harsha14gupta@users.noreply.github.com>
2020-05-23 13:16:42 +0530

    Update and rename positive-integer.js to positive-float-integer.js

--- src/validation/positive-integer.js
+++
@@ -1,8 +0,0 @@
-/*
- * Positive Integer Validation
- */
-
-module.exports = (arg, func) => {
-	if(typeof arg != 'number' || arg + 1 == arg || arg != arg || arg < 1 || !Number.isInteger(arg))
-		throw new TypeError(`Invalid argument received -  ${JSON.stringify(arg)}\n'${func}()' only accept a positive integer!\n`);
-}
