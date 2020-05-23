f97d66011187dc3ae5b872d3c0b133dbf27e61a5
Harsha <56157392+harsha14gupta@users.noreply.github.com>
2020-05-23 13:12:08 +0530

    Update index.js

--- src/gcd/index.js
+++ src/gcd/index.js
@@ -1,12 +1,27 @@
 /* Function: gcd() */
 
-const validate = require('../validation/integer-array');
+const validate = require('../validation/positive-float-integer-array');
 
 function gcd(a, b) {
-  return (a == 0) ? b : gcd(b % a, a);
+  	if (a < b) {
+		return gcd(b, a);
+	}
+	return (Math.abs(b) < 0.001) ? a : gcd(b, a - Math.floor(a / b) * b);
+}
+
+function countDecimals(value) {
+	if (value === 0 || Number.isInteger(value)) {
+		return 0;
+	}
+	else {
+	return value.toString().split(".")[1].length || 0;
+  }
 }
 
 module.exports = arr => {
 	validate(arr, 'gcd');
-	return arr.reduce((prev, next) =>  gcd(prev, next), arr[0]);
+	let sum = Number(arr.reduce((prev, next) => prev + next).toFixed(10));
+	return Math.round((arr.reduce((prev, next) => 
+				gcd(prev, next), arr[0])) * Math.pow(10, countDecimals(sum))) / Math.pow(10, countDecimals(sum));
+};
 };
